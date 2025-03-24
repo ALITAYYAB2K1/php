462ae213c545ff07d05c3ca014291d9a8134667c
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-03-24 05:58:24 +0500

    php basic cleared

---
+++ includesphp/about.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require 'header.php'; 
+     // if we use require instead of include, the file must exist and be valid or the script will stop executing
+     // if we use include, the file does not have to exist and the script will continue executing
+    ?>
+    
+
+
+    <main>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+    </main>
+    <?php include 'footer.php'; ?>
+
+
+</body>
+</html>
